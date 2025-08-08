c2ecd65ce00ebdac867f3e3d6da40028a92e05c7
Aditi-Anil-P <anijasadi2@gmail.com>
2025-08-08 10:57:20 +0530

    Added single compare feature

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template,request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from auth import auth_bp, db, User  # imported from auth
 from file_handler import read_file
+from single_compare import generate_single_compare_chart, detect_valid_data
 
 
 app = Flask(__name__)
@@ -36,11 +37,32 @@ def upload_file():
     file = request.files['csv_file']
     file_path = os.path.join('uploads', file.filename)
     file.save(file_path)
-
+    session['uploaded_file_path'] = file_path
     df = read_file(file_path)
     print(df.head())  # For testing purposes
+    return redirect('/single_compare')
 
-    return "File uploaded and read successfully"
+@app.route('/single_compare', methods=['GET', 'POST'])
+def single_compare():
+    file_path = session.get('uploaded_file_path')  
+
+    if not file_path:
+        return "No uploaded file found in session."  
+
+    if request.method == 'POST':
+        parameter = request.form['parameter']  
+        preference = request.form['preference']  # 'higher' or 'lower'
+
+        try:
+            df, numeric_df = detect_valid_data(file_path)  
+            chart_path = generate_single_compare_chart(df, numeric_df, parameter, preference=preference)
+            return render_template('result.html', chart_path=chart_path)
+        except Exception as e:
+            return f"Error processing chart: {str(e)}"
+
+
+    return render_template('single_compare.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
